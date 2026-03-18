ba92a2f37e1049649d90ea954f63e559937dffda
Prachiti Sakpal <sakpalprachiti2@gmail.com>
2026-03-18 21:17:43 +0530

    Day 1 - Hello World Python

---
+++ 01-python-basics/day01_hello_world.py
@@ -0,0 +1,35 @@
+#Day 1 -Hello world & Python basics
+#Author:Prachiti Sakpal
+#Date:18th March 2026
+
+#Part 1: Hello World----
+
+print(" Hello World")
+
+
+#Part 2: About Me----
+print("\n About Me")
+name = "Prachiti Rakesh Sakpal"
+age = 18
+city = "Dombivli"
+college = "Vasantdada Patil College Of Engineering"
+course = "AIML Engineering"
+goal = "Become a Machine Learning Engineer"
+
+print(f"Name   :{name}")
+print(f"Age    :{age}")
+print(f"City   :{city}")
+print(f"College:{college}")
+print(f"Course :{course}")
+print(f"Goal   :{goal}")
+
+#Part 3:About Math----
+print("\n About Math")
+a=20
+b=8
+
+print(f"{a}+{b} = {a+b}")
+print(f"{a}-{b} = {a-b}")
+print(f"{a}*{b} = {a*b}")
+print(f"{a}/{b} = {a/b}")
+print(f"{a}%{b} = {a%b}")
